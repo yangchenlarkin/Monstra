2e744e912afe09260ccf191cfa1d9da61810e0ca
Larkin Yang <Larkin.Yang@zennioptical.com>
2025-07-17 21:53:37 +0800

    chore: update TTLPriorityLRUQueueTests to remove unused variable and match new method names

--- Sources/Monstore/MemoryCache/TTLPriorityLRUQueue.swift
+++ Sources/Monstore/MemoryCache/TTLPriorityLRUQueue.swift
@@ -49,33 +49,32 @@ class TTLPriorityLRUQueue<Key: Hashable, Value> {
 
 extension TTLPriorityLRUQueue {
     @discardableResult
-    func unsafeSet(value: Value, for key: Key, priority: Double = .zero, expiredIn duration: TimeInterval = .infinity) -> Value? {
+    func set(value: Value, for key: Key, priority: Double = .zero, expiredIn duration: TimeInterval = .infinity) -> Value? {
         let now = CPUTimeStamp.now()
-
         // Check if the key already exists in the LRU queue
-        if unsafeRemoveValue(for: key) != nil {
-            return unsafeSetToLRUQueue(now: now, value: value, for: key, priority: priority, expiredIn: duration)
+        if removeValue(for: key) != nil {
+            return setToLRUQueue(now: now, value: value, for: key, priority: priority, expiredIn: duration)
         } else {
             // If the key does not exist, check if the TTL heap root has expired
             if let ttlRoot = ttlQueue.root?.expirationTimeStamp, ttlRoot < now {
                 // If the root of TTL heap is expired, insert into the TTL heap
-                return unsafeSetToTTLQueue(now: now, value: value, for: key, priority: priority, expiredIn: duration)
+                return setToTTLQueue(now: now, value: value, for: key, priority: priority, expiredIn: duration)
             }
             // Otherwise, insert into the LRU queue
-            return unsafeSetToLRUQueue(now: now, value: value, for: key, priority: priority, expiredIn: duration)
+            return setToLRUQueue(now: now, value: value, for: key, priority: priority, expiredIn: duration)
         }
     }
     
     func getValue(for key: Key) -> Value? {
         guard let node = lruQueue.getValue(for:key) else { return nil }
         if node.expirationTimeStamp < .now() {
-            _=unsafeRemoveValue(for: key)
+            _=removeValue(for: key)
             return nil
         }
         return node.value
     }
     
-    func unsafeRemoveValue(for key: Key) -> Value? {
+    func removeValue(for key: Key) -> Value? {
         if let node = lruQueue.removeValue(for: key) {
             if let nodeIndex = node.ttlIndex {
                 _=ttlQueue.remove(at: nodeIndex)
@@ -88,7 +87,7 @@ extension TTLPriorityLRUQueue {
 }
 
 private extension TTLPriorityLRUQueue {
-    func unsafeSetToTTLQueue(now: CPUTimeStamp, value: Value, for key: Key, priority: Double, expiredIn duration: TimeInterval) -> Value? {
+    func setToTTLQueue(now: CPUTimeStamp, value: Value, for key: Key, priority: Double, expiredIn duration: TimeInterval) -> Value? {
         var res: Value? = nil
         let node = Node(key: key, value: value, expirationTimeStamp: now + duration)
         if let pop = ttlQueue.insert(node, force: true) {
@@ -99,7 +98,7 @@ private extension TTLPriorityLRUQueue {
         return res
     }
     
-    func unsafeSetToLRUQueue(now: CPUTimeStamp, value: Value, for key: Key, priority: Double, expiredIn duration: TimeInterval) -> Value? {
+    func setToLRUQueue(now: CPUTimeStamp, value: Value, for key: Key, priority: Double, expiredIn duration: TimeInterval) -> Value? {
         var res: Value? = nil
         let node = Node(key: key, value: value, expirationTimeStamp: now + duration)
         if let pop = lruQueue.setValue(node, for: key, with: priority), let ttlIndex = pop.ttlIndex {

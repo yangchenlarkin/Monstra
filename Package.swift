7ea033c91733a403ee26022f2491afb430af13f0
Larkin <yangchenlarkin@gmail.com>
2025-07-19 19:48:29 +0800

    fix: correct sublibrary name to Monstore (capitalized)

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "monstore",
-            targets: ["monstore"]),
+            name: "Monstore",
+            targets: ["Monstore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,15 +25,15 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "monstore",
+            name: "Monstore",
             dependencies: [],
             swiftSettings: [
                 .define("DEBUG", .when(configuration: .debug)),
                 .unsafeFlags(["-enable-testing"], .when(configuration: .debug))
             ]),
         .testTarget(
-            name: "monstoreTests",
-            dependencies: ["monstore"],
+            name: "MonstoreTests",
+            dependencies: ["Monstore"],
             swiftSettings: [
                 .define("DEBUG", .when(configuration: .debug))
             ]),
